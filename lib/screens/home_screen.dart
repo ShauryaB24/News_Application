8d09422240a20c3614d6a87aa729e67754aa29c1
Shaurya Bansal <shauryab22@gmail.com>
2024-07-04 21:12:45 +0530

    Newz_app

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:news_app_flutter_course/services/utils.dart';
+import 'package:news_app_flutter_course/widgets/drawer_widget.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/theme_provider.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final Color color = Utils(context).getColor;
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          iconTheme: IconThemeData(color: color),
+          elevation: 0,
+          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+          centerTitle: true,
+          title: Text(
+            'News app',
+            style: GoogleFonts.lobster(
+                textStyle:
+                    TextStyle(color: color, fontSize: 20, letterSpacing: 0.6)),
+          ),
+          actions: [
+            IconButton(onPressed: () {}, icon: const Icon(IconlyLight.search))
+          ],
+        ),
+        drawer: const DrawerWidget(),
+        body: Container(),
+      ),
+    );
+  }
+}
