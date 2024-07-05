dedb78d2219e9103727728d9495757b1cda69bf7
Shaurya Bansal <shauryab22@gmail.com>
2024-07-05 13:15:41 +0530

    All news button

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -38,7 +38,28 @@ class _HomeScreenState extends State<HomeScreen> {
           ],
         ),
         drawer: const DrawerWidget(),
-        body: Container(),
+        body: Column(children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: 
+            Row(
+              children: [
+              GestureDetector(
+                onTap: (){},
+                child: Container(
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(15),color: Theme.of(context).cardColor),
+                
+                child: const Padding(
+                  padding:  EdgeInsets.all(8.0),
+                  child: 
+                  Text('All News', 
+                  style: TextStyle(
+                    fontSize: 22),),
+                )),
+              )
+            ],),
+          )
+        ],),
       ),
     );
   }
